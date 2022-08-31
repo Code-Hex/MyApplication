9f394b10508bec560927dda7a0e5abdcc387a816
Kei Kamikawa <code-hex@users.noreply.github.com>
2022-09-01 00:40:36 +0900

    Initial Commit

---
+++ MyApplication/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MyApplication
+//
+//  Created by Kei Kamikawa on 2022/09/01.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MyApplication/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  MyApplication
+//
+//  Created by Kei Kamikawa on 2022/09/01.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
